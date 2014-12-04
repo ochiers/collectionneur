1375ef23f1ddb628d2678e884bc1a706ae11f921
ochiers <ochiers>
2014-12-05 00:38:33 +0100

    Based the project

---
+++ collectionneur/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+        <script type="text/javascript" src="js/main.js"></script>
+        <title>Le collectionneur</title>
+    </head>
+    <body>
+        <div id="header">
+            <h1>Le collectionneur.</h1>
+        </div>
+        <div id="center">
+            <div id="avatar">
+                
+            </div>
+            <div id="name">
+                
+            </div>
+            <div id="info">
+                
+            </div>
+        </div>
+        <div id="footer">
+            
+        </div>
+    </body>
+</html>
