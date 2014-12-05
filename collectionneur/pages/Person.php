d2caa3421bc4bfb5d6e3b46d373c62bab64c4fd2
loucasse93 <loucasse93@hotmail.fr>
2014-12-05 04:55:16 +0100

    Recup DB person

--- collectionneur/pages/Person.php
+++ collectionneur/pages/Person.php
@@ -4,23 +4,46 @@
 			$var = '<img src="images/person0.png"/>';
 		} else {
 			$var = '<img src="images/person'.$_GET['id'].'.png"/>';
-		}
+		
 
 		echo $var;
-    
-
-	include("sql/connect.php");
-	$bdd = connexion_db();
-
-	//
-
     ?>
-
-
 </div>
 <div id="name">
     Jean Jacques
 </div>
-<div id="info">
-   Virus
-</div>
+ <?php
+
+	$idPerson = (int)htmlspecialchars($_GET['id']);
+
+
+	include("sql/connect.php");
+	$bdd = connexion_db();
+
+	//Récupération des tous les items de la personne $idPerson
+	$requete = $bdd->prepare('SELECT * FROM Person WHERE IdPerson = ?');
+	$requete->execute(array($idPerson)) or die (print_r($requete->errorInfo()));
+
+	$ligneItem = $requete->fetch(PDO::FETCH_ASSOC);
+
+	echo '<div id="intro">';
+	echo '<table>';
+	
+	$i=1;
+	do
+	{
+		echo '<tr>';
+
+			echo '<td>Item '.$i.'</td>';
+			echo '<td>'.$ligneItem['IdItem'].'</td>';
+
+		echo '</tr>';
+		$i = $i +1;
+	} while($ligneItem=$requete->fetch(PDO::FETCH_ASSOC));
+
+	echo '</table>';
+	echo '</div>';
+	
+	}
+
+	?>
