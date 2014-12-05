afa720e63775630e47d93df578c4d54cf592ca96
loucasse93 <loucasse93@hotmail.fr>
2014-12-05 01:49:37 +0100

    Commit original de l'index

--- collectionneur/index.php
+++ collectionneur/index.php
@@ -17,13 +17,13 @@ and open the template in the editor.
         </div>
         <div id="center">
             <div id="avatar">
-                
+                <img src="images/person0.png"/>
             </div>
             <div id="name">
-                
+                Jean Jacques
             </div>
             <div id="info">
-                
+                Virus
             </div>
         </div>
         <div id="footer">
